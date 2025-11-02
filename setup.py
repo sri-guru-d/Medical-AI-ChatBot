8eb88a2a36c34c6ea518239efb7aee95ae964b09
sri-guru-d <d.sriguru@gmail.com>
2025-11-03 00:25:23 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Medi_Bot",
+    version="0.1.0",
+    author="Sri Gurubaghuvela",
+    author_email="d.sriguru@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
